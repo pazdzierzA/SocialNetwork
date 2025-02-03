6a51a7740060464e5f3e33610a03ff9654f64274
pazdzierzA <apazdzierz@laba.solvd.com>
2025-02-03 22:51:24 +0100

    Small changes

--- Implementation/socialnetwork/src/main/java/com/solvd/socialnetwork/services/IEventService.java
+++ Implementation/socialnetwork/src/main/java/com/solvd/socialnetwork/services/IEventService.java
@@ -3,7 +3,6 @@ package com.solvd.socialnetwork.services;
 import java.util.List;
 
 import com.solvd.socialnetwork.models.Event;
-import com.solvd.socialnetwork.services.impl.EventService;
 
 public interface IEventService extends IService<Event>{
 	List<Event> getByName (String name);
--- Implementation/socialnetwork/src/main/java/com/solvd/socialnetwork/services/IGroupMemberService.java
+++ Implementation/socialnetwork/src/main/java/com/solvd/socialnetwork/services/IGroupMemberService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.solvd.socialnetwork.enums.UserRole;
 import com.solvd.socialnetwork.models.GroupMember;
 import com.solvd.socialnetwork.models.User;
-import com.solvd.socialnetwork.services.impl.GroupMemberService;
 
 public interface IGroupMemberService extends IService<GroupMember>{
 	List<GroupMember> getByUserRole(UserRole userRole);
