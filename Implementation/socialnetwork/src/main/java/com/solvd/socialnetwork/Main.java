531f36500604420b666fe49cff3c9059e9a20e37
pazdzierzA <apazdzierz@laba.solvd.com>
2025-02-14 11:46:55 +0100

    builder and lambok builder

--- Implementation/socialnetwork/src/main/java/com/solvd/socialnetwork/Main.java
+++ Implementation/socialnetwork/src/main/java/com/solvd/socialnetwork/Main.java
@@ -22,6 +22,7 @@ import com.solvd.socialnetwork.models.LikePost;
 import com.solvd.socialnetwork.models.Post;
 import com.solvd.socialnetwork.models.Story;
 import com.solvd.socialnetwork.models.User;
+import com.solvd.socialnetwork.services.impl.StoryService;
 import com.solvd.socialnetwork.services.impl.UserService;
 import com.solvd.socialnetwork.services.parsers.SAXLocal;
 
@@ -36,34 +37,44 @@ import com.solvd.socialnetwork.models.Comment;
 
 public class Main {
 	private final static Logger logger = LogManager.getLogger(Main.class.getName());
+	private static final String XML_FILE_PATH = "src/main/resources/xmls/social_networks.xml";
+	private static final String POSTS_XML_FILE_PATH = "src/main/resources/xmls/posts.xml";
+	private static final String JSON_FILE_PATH = "src/main/resources/jsons/social_network.json";
+	private static final String OUTPUT_JSON_FILE_PATH = "src/main/resources/jsons/social_networks.json";
+	private static final String POSTS_JSON_FILE_PATH = "src/main/resources/jsons/posts.json";
 
 	public static void main(String[] args) {
-		
+
 		LocalDate userDate = LocalDate.of(1990, 05, 15);
-		//User userJohn = new User("john_doe","john.doe@example.com","password123","John","Doe",userDate);
+		//builder pattern
+		User userAdam = User.builder().login("adam123").email("adam_oak@example.com").password("password123")
+				.firstName("Adam").lastName("Oak").birthDate(userDate).build();
+
+		// lombok builder
+		Story firstStory = Story.builder().text("This is my first story").pictureStoryUrl("url").storyCreatorId(1L).build();
+		
+		// service pattern
+		StoryService storyService = new StoryService();
+		storyService.save(firstStory);
+		
+		// User userJohn = new
+		// User("john_doe","john.doe@example.com","password123","John","Doe",userDate);
 		UserService userService = new UserService();
-		User updatedJohn = new User((long)29,"Adam_doe","Adam.doe@example.com","password123","Adam","Doe",userDate);
-       	//userJohn = userService.save(userJohn);
+		User updatedJohn = new User((long) 29, "Adam_doe", "Adam.doe@example.com", "password123", "Adam", "Doe",
+				userDate);
 
-       //	List<User> withNameJohn = userService.getByFirstName("John");
-       	//List<User> withNameAdam = userService.getByFirstName("Adam");
-       	
-       	/*for (User john: withNameJohn) {
-       	logger.info("Users from db with name John: {}", john.toString());
-       	}*/
-       	
-       	userService.update(updatedJohn);
-       	userService.removeById((long) 31);
+		userService.save(userAdam);
+		userService.update(updatedJohn);
+		userService.removeById((long) 31);
 		userService.removeById((long) 33);
-       	/*for (User john: withNameJohn) {
-           	logger.info("Users from db with name John after changes : {}", john.toString());
-           	}*/
-     	/*for (User adam: withNameAdam) {
-           	logger.info("Users from db with name Adam after changes : {}", adam.toString());
-           	}*/
 
-		/*// SAX Parser
-    	logger.info("SAX parser value: ");
+		parseSAX();
+		parseJAXB();
+		parseJackson();
+	}
+
+	private static void parseSAX() {
+		logger.info("SAX parser value: ");
 		List<User> parsedUsers = new ArrayList<>();
 		List<Post> parsedPosts = new ArrayList<>();
 		List<Story> parsedStories = new ArrayList<>();
@@ -72,7 +83,6 @@ public class Main {
 		List<Comment> parsedComments = new ArrayList<>();
 		List<GroupMember> parsedGroupMembers = new ArrayList<>();
 
-		InputSource inputFile = new InputSource("src/main/resources/xmls/social_networks.xml");
 		SAXParserFactory spf = SAXParserFactory.newInstance();
 		spf.setNamespaceAware(true);
 		SAXLocal saxLocal = new SAXLocal();
@@ -81,7 +91,8 @@ public class Main {
 			SAXParser saxParser = spf.newSAXParser();
 			XMLReader xmlReader = saxParser.getXMLReader();
 			xmlReader.setContentHandler(saxLocal);
-			xmlReader.parse(inputFile);
+			xmlReader.parse(new InputSource(XML_FILE_PATH));
+
 			parsedUsers = saxLocal.getUsers();
 			parsedPosts = saxLocal.getPosts();
 			parsedStories = saxLocal.getStories();
@@ -89,99 +100,78 @@ public class Main {
 			parsedLikes = saxLocal.getLikes();
 			parsedComments = saxLocal.getComments();
 			parsedGroupMembers = saxLocal.getGroupMembers();
-			for (User user : parsedUsers) {
-				logger.info(user.toString());
-			}
-			for (Post post : parsedPosts) {
-				logger.info(post.toString());
-			}
 
-			for (Story story : parsedStories) {
-				logger.info(story.toString());
-			}
-
-			for (Group group : parsedGroups) {
-				logger.info(group.toString());
-			}
-
-			for (LikePost likePost : parsedLikes) {
-				logger.info(likePost.toString());
-			}
-			for (Comment comment : parsedComments) {
-				logger.info(comment.toString());
-			}
-
-			for (GroupMember groupMember : parsedGroupMembers) {
-				logger.info(groupMember.toString());
-			}
-		} catch (ParserConfigurationException e) {
-			logger.error("Parser configuration error: {}", e.getMessage(), e);
-		} catch (SAXException e) {
-			 logger.error("Error while parsing XML: {}", e.getMessage(), e);
-		} catch (IOException e) {
-			 logger.error("Error while parsing XML: {}", e.getMessage(), e);
+			logParsedData(parsedUsers, parsedPosts, parsedStories, parsedGroups, parsedLikes, parsedComments,
+					parsedGroupMembers);
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			logger.error("Error while parsing XML: {}", e.getMessage(), e);
 		}
-		
-		
-		// JAXB 
+	}
+
+	private static void logParsedData(List<User> users, List<Post> posts, List<Story> stories, List<Group> groups,
+			List<LikePost> likes, List<Comment> comments, List<GroupMember> groupMembers) {
+		users.forEach(user -> logger.info(user.toString()));
+		posts.forEach(post -> logger.info(post.toString()));
+		stories.forEach(story -> logger.info(story.toString()));
+		groups.forEach(group -> logger.info(group.toString()));
+		likes.forEach(like -> logger.info(like.toString()));
+		comments.forEach(comment -> logger.info(comment.toString()));
+		groupMembers.forEach(groupMember -> logger.info(groupMember.toString()));
+	}
+
+	private static void parseJAXB() {
 		logger.info("JAXB parser value: ");
-		PostWrapper result = null;
-		
-        try {
-        	InputSource inputFilePost = new InputSource("src/main/resources/xmls/posts.xml");
-        	JAXBContext context = JAXBContext.newInstance(PostWrapper.class);
-			Marshaller marshaller = context.createMarshaller();
+		try {
+			JAXBContext context = JAXBContext.newInstance(PostWrapper.class);
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-			result = (PostWrapper) unmarshaller.unmarshal( inputFilePost);
+			PostWrapper result = (PostWrapper) unmarshaller.unmarshal(new InputSource(POSTS_XML_FILE_PATH));
+
 			for (Post post : result.getPosts()) {
 				logger.info("Title: {}", post.getTitle());
-                logger.info("Text: {}", post.getText());
-                logger.info("Likes: {}", post.getLikeQuantity());
-                logger.info("Comments: {}", post.getCommentQuantity());
-                logger.info("--------------------------------");
-            }
-			
+				logger.info("Text: {}", post.getText());
+				logger.info("Likes: {}", post.getLikeQuantity());
+				logger.info("Comments: {}", post.getCommentQuantity());
+				logger.info("--------------------------------");
+			}
 		} catch (JAXBException e) {
-			
-			logger.error(e);
+			logger.error("Error while parsing JAXB: {}", e.getMessage(), e);
 		}
-        
-        
-        //JACKSON
-        
-        try {
-        	ObjectMapper objectMapper = new ObjectMapper();
-        	objectMapper.registerModule(new JavaTimeModule());
-			SocialNetwork socialNetworkData = objectMapper.readValue(new File("src/main/resources/jsons/social_network.json"), SocialNetwork.class);
-			objectMapper.writeValue(new File("src/main/resources/jsons/social_networks.json"), socialNetworkData);
-			
-			
-			Post post = new Post((long)1,"This is my first post!","Introduction",20, 5,(long)1 );
-			Post secondPost = new Post((long)2,"This is my second post!","Second day",10, 6,(long)2 );
-			Comment firstComment = new Comment((long) 1,"Fantastic",(long)1,(long)2);
-			Comment secondComment = new Comment((long) 2,"Bad",(long)2,(long)2);
-			LikePost like = new LikePost((long)1, (long)2,(long)1);
-			LikePost secondLike = new LikePost((long)2, (long)2,(long)2);
+	}
+
+	private static void parseJackson() {
+		logger.info("JACKSON parser value: ");
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.registerModule(new JavaTimeModule());
+			SocialNetwork socialNetworkData = objectMapper.readValue(new File(JSON_FILE_PATH), SocialNetwork.class);
+			objectMapper.writeValue(new File(OUTPUT_JSON_FILE_PATH), socialNetworkData);
+
+			Post post = new Post(1L, "This is my first post!", "Introduction", 20, 5, 1L);
+			Post secondPost = new Post(2L, "This is my second post!", "Second day", 10, 6, 2L);
+			Comment firstComment = new Comment(1L, "Fantastic", 1L, 2L);
+			Comment secondComment = new Comment(2L, "Bad", 2L, 2L);
+			LikePost like = new LikePost(1L, 2L, 1L);
+			LikePost secondLike = new LikePost(2L, 2L, 2L);
+
 			List<Comment> comments = new ArrayList<>();
 			comments.add(firstComment);
 			comments.add(secondComment);
+
 			List<LikePost> likes = new ArrayList<>();
 			likes.add(like);
 			likes.add(secondLike);
-			List <Post> allPosts = new ArrayList<>();
+
+			List<Post> allPosts = new ArrayList<>();
 			allPosts.add(post);
 			allPosts.add(secondPost);
+
 			secondPost.setComments(comments);
 			secondPost.setLikes(likes);
-			
-			//ObjectMapper objectMapper1 = new ObjectMapper();
-			objectMapper.writeValue(new File("src/main/resources/jsons/posts.json"), allPosts);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        */
-        
 
+			objectMapper.writeValue(new File(POSTS_JSON_FILE_PATH), allPosts);
+		} catch (IOException e) {
+			logger.error("Error while parsing JSON: {}", e.getMessage(), e);
+		}
 	}
+
 }
--- Implementation/socialnetwork/src/main/java/com/solvd/socialnetwork/models/Story.java
+++ Implementation/socialnetwork/src/main/java/com/solvd/socialnetwork/models/Story.java
@@ -1,8 +1,14 @@
 package com.solvd.socialnetwork.models;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Builder
+@EqualsAndHashCode(callSuper = true)
+
 public class Story extends BaseEntity {
-
-
 	private String text;
 	private String pictureStoryUrl;
 	private Long storyCreatorId;
@@ -14,11 +20,17 @@ public class Story extends BaseEntity {
 		this.storyCreatorId = storyCreatorId;
 	}
 
+	public Story(String text, String pictureStoryUrl, Long storyCreatorId) {
+		this.text = text;
+		this.pictureStoryUrl = pictureStoryUrl;
+		this.storyCreatorId = storyCreatorId;
+	}
+
 	public Story() {
 
 	}
 
-	public String getText() {
+	/*public String getText() {
 		return text;
 	}
 
@@ -46,6 +58,6 @@ public class Story extends BaseEntity {
 	public String toString() {
 		return "Story [text=" + text + ", pictureStoryUrl=" + pictureStoryUrl + ", storyCreatorId=" + storyCreatorId
 				+ "]";
-	}
+	}*/
 
 }
